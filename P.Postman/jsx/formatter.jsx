59084459c7a725b02d5b49f0183fea26372e8184
cdriesler <cdriesler.iv@gmail.com>
2018-12-22 02:40:24 -0500

    version 1.0.0

--- P.Postman/jsx/formatter.jsx
+++ P.Postman/jsx/formatter.jsx
@@ -1,5 +1,27 @@
 ﻿var targetDoc = app.activeDocument;
 var pgs = targetDoc.pages;
 
-for
+var dataFile = new File("C:\\Users\\cdrie\\systems\\pesusich\\P.Postman\\sanitized.txt")
+
+dataFile.open("r");
+var data = dataFile.read().split("\n");
+
+//$.writeln(data[1])
+
+//var testFrame = pgs[0].textFrames[0];
+//testFrame.contents = data[0] + "\n" + data[1] + "\n" + data[2];
+//$.writeln(testFrame.contents);
+
+for (i = 0; i < 121; i++) {
+    var activePgIndex = Math.floor (i / 30);
+    var activePg = pgs[activePgIndex];
+    
+    var frames = activePg.textFrames;
+    var activeFrameIndex = i % 30;
+    var activeFrame = frames[activeFrameIndex];
+    
+    var baseIndex = i * 3;
+    
+    activeFrame.contents = data[baseIndex] + "\n" + data[baseIndex + 1] + "\n" + data[baseIndex + 2];
+    }
 
